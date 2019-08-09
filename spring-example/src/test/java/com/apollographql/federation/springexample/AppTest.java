74a0dee864f1dfab404ce5373c9a9340d60813c1
Adam Zionts <adam@meteor.com>
2019-08-09 13:53:49 -0700

    Add test for checking result of SDL call

--- spring-example/src/test/java/com/apollographql/federation/springexample/AppTest.java
+++ spring-example/src/test/java/com/apollographql/federation/springexample/AppTest.java
@@ -28,4 +28,14 @@ public class AppTest {
         final int quantity = response.get("$.data._entities[0].quantity", Integer.class);
         assertEquals(8658, quantity, "Inventory contains 8658 Plancks");
     }
+
+    @Test
+    public void sdlTest() throws IOException {
+        final GraphQLResponse response = graphqlTestTemplate.postForResource("queries/ServiceSdlCall.graphql");
+        assertNotNull(response, "response should not have been null");
+        assertTrue(response.isOk(), "response should have been OK");
+        System.out.println(response.get("$.data._service.sdl"));
+        // final int sdl = response.get("$.data._service.sdl", String.class);
+        // assertEquals("not equal", sdl, "SDL is what you expect");
+    }
 }
